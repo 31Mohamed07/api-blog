0c783719a67c9ef11c75059f1a8271b983825fb2
MOHAMED <mounchilimohamed45@gmail.com>
2026-03-23 10:00:40 +0100

    Ajout documentation Swagger

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 
 dotenv.config();
-
 const app = express();
 app.use(express.json());
 app.use(express.static('public'));
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Blog Mohamed SARL',
+      version: '1.0.0',
+      description: 'Documentation de l\'API REST pour la gestion des articles de blog',
+    },
+    servers: [
+      { url: 'http://localhost:3000/api', description: 'Serveur local' },
+      { url: 'https://api-blog-production-8f83.up.railway.app/api', description: 'Serveur Railway' },
+    ],
+  },
+  apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const articlesRoutes = require('./routes/articles');
 app.use('/api/articles', articlesRoutes);
@@ -14,5 +34,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur le port ${PORT}`);
 });
-
 module.exports = app;
--- routes/articles.js
+++ routes/articles.js
@@ -1,12 +1,155 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/articleController');
+const articleController = require('../controllers/articleController');
 
-router.get('/search', controller.searchArticles);
-router.get('/', controller.getArticles);
-router.get('/:id', controller.getArticleById);
-router.post('/', controller.createArticle);
-router.put('/:id', controller.updateArticle);
-router.delete('/:id', controller.deleteArticle);
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Article:
+ *       type: object
+ *       required:
+ *         - titre
+ *         - contenu
+ *         - auteur
+ *       properties:
+ *         id:
+ *           type: integer
+ *         titre:
+ *           type: string
+ *         contenu:
+ *           type: string
+ *         auteur:
+ *           type: string
+ *         categorie:
+ *           type: string
+ *         tags:
+ *           type: string
+ *         date:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * /articles:
+ *   get:
+ *     summary: Récupérer tous les articles
+ *     parameters:
+ *       - in: query
+ *         name: categorie
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: auteur
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: date
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Liste des articles
+ */
+router.get('/', articleController.getArticles);
+
+/**
+ * @swagger
+ * /articles/search:
+ *   get:
+ *     summary: Rechercher des articles
+ *     parameters:
+ *       - in: query
+ *         name: query
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Résultats de recherche
+ */
+router.get('/search', articleController.searchArticles);
+
+/**
+ * @swagger
+ * /articles/{id}:
+ *   get:
+ *     summary: Récupérer un article par ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Article trouvé
+ *       404:
+ *         description: Article non trouvé
+ */
+router.get('/:id', articleController.getArticleById);
+
+/**
+ * @swagger
+ * /articles:
+ *   post:
+ *     summary: Créer un article
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Article'
+ *     responses:
+ *       201:
+ *         description: Article créé
+ *       400:
+ *         description: Champs manquants
+ */
+router.post('/', articleController.createArticle);
+
+/**
+ * @swagger
+ * /articles/{id}:
+ *   put:
+ *     summary: Modifier un article
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Article'
+ *     responses:
+ *       200:
+ *         description: Article modifié
+ *       404:
+ *         description: Article non trouvé
+ */
+router.put('/:id', articleController.updateArticle);
+
+/**
+ * @swagger
+ * /articles/{id}:
+ *   delete:
+ *     summary: Supprimer un article
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Article supprimé
+ *       404:
+ *         description: Article non trouvé
+ */
+router.delete('/:id', articleController.deleteArticle);
 
 module.exports = router;
